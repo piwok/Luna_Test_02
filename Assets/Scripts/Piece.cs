9dcbb0de4b0187849c66d27e0d161a26a27a9ce2
Julio Menendez <juliomenendezdiaz@gmail.com>
2023-12-26 17:26:47 +0100

    mouse interactivity working, calculateAngle function is working

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Piece : MonoBehaviour
 {
+    private Vector2 firstTouchPosition;
+    private Vector2 lastTouchPosition;
+    public float swipeAngle;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,18 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnMouseDown() {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(firstTouchPosition);
+    }
+    private void OnMouseUp() {
+        lastTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(lastTouchPosition);
+        calculateAngle();
+    }
+    private void calculateAngle() {
+        swipeAngle = Mathf.Atan2(lastTouchPosition.y - firstTouchPosition.y, lastTouchPosition.x - firstTouchPosition.x) * 180/Mathf.PI;
+        Debug.Log(swipeAngle);
     }
 }

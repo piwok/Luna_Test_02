9272f35f9a2227d35146f7b7466cee700536ff40
Julio Menendez <juliomenendezdiaz@gmail.com>
2023-12-27 19:43:37 +0100

    basic destroy matches working without bugs detected

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Board : MonoBehaviour
 {
@@ -28,10 +29,58 @@ public class Board : MonoBehaviour
                 newTile.GetComponent<Tile>().row = j;
                 allTiles[i, j] = newTile;
                 int newRegularPieceTypeIndex = Random.Range(0, piecesPrefabs.Length);
+                int maxIterations = 0;
+                while(isMatchAt(i, j, piecesPrefabs[newRegularPieceTypeIndex]) && maxIterations < 100) {
+                    newRegularPieceTypeIndex = Random.Range(0, piecesPrefabs.Length);
+                    maxIterations++;
+                }
                 GameObject newRegularPiece = Instantiate(piecesPrefabs[newRegularPieceTypeIndex], positionForNewPiece, Quaternion.identity);
                 allPieces[i, j] = newRegularPiece;
-
             }
         }
+    }
+    private bool isMatchAt(int column, int row, GameObject exploringPiece) {
+        if(column > 1 && row > 1) {
+            if(allPieces[column -1, row].GetComponent<Piece>().color == exploringPiece.GetComponent<Piece>().color &&
+            exploringPiece.GetComponent<Piece>().color == allPieces[column - 2, row].GetComponent<Piece>().color) {
+                return true;
+            }
+            if(allPieces[column, row - 1].GetComponent<Piece>().color == exploringPiece.GetComponent<Piece>().color &&
+            exploringPiece.GetComponent<Piece>().color == allPieces[column, row - 2].GetComponent<Piece>().color) {
+                return true;
+            }
+        }
+        else if(column <= 1 || row <= 1) {
+            if(row > 1) {
+                if(allPieces[column, row - 1].GetComponent<Piece>().color == exploringPiece.GetComponent<Piece>().color &&
+                allPieces[column, row - 2].GetComponent<Piece>().color == exploringPiece.GetComponent<Piece>().color) {
+                    return true;
+                }
+            }
+            if(column > 1) {
+                if(allPieces[column - 1, row].GetComponent<Piece>().color == exploringPiece.GetComponent<Piece>().color &&
+                allPieces[column - 2, row].GetComponent<Piece>().color == exploringPiece.GetComponent<Piece>().color) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+    private void destroyMatchesAt(int column, int row) {
+        if(allPieces[column, row].GetComponent<Piece>().isMatched) {
+            Destroy(allPieces[column, row]);
+            allPieces[column, row] = null;
+        }
+    }
+    public void destrolAllMatches() {
+        for(int i = 0; i < width;i++) {
+            for(int j = 0; j < height; j++) {
+                if(allPieces[i, j] != null) {
+                    destroyMatchesAt(i, j);
+                }
+            }
+        }
+
     }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {   
+    [Header("Board variables")]
     public string color;
     public string type;
     public int column;
     public int row;
+    public int previousColumn;
+    public int previousRow;
     private int targetColumn;
     private int targetRow;
     private Vector2 targetPosition;
@@ -17,7 +20,7 @@ public class Piece : MonoBehaviour
     private Vector2 firstTouchPosition;
     private Vector2 lastTouchPosition;
     public float swipeAngle;
-    private GameObject otherPiece;
+    public float swipeThreshold;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +28,9 @@ public class Piece : MonoBehaviour
         targetRow = (int) transform.position.y;
         column = targetColumn;
         row = targetRow;
+        previousColumn = column;
+        previousRow = row;
+        swipeThreshold = 1f;
         isMatched = false;
         board = FindObjectOfType<Board>();
         
@@ -32,13 +38,22 @@ public class Piece : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   //Code for the movement of the piece, the piece always goes to the position of the column and row variables
+    {   //Code for destruction of matched pieces
+        
+        if (isMatched) {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            sprite.color = new Color(1f, 1f, 1f, 0.4f);
+        }
+        //Code for the movement of the piece, the piece always goes to the position of the column and row variables
         targetColumn = column;
         targetRow = row;
         if (Mathf.Abs(targetColumn - transform.position.x) > 0.1f) {
             //Move towards the target
             targetPosition = new Vector2(targetColumn, transform.position.y);
             transform.position = Vector2.Lerp(transform.position, targetPosition, 17f*Time.deltaTime);
+            if(board.allPieces[column, row] != this.gameObject) {
+                board.allPieces[column, row] = this.gameObject;
+            }
         }
         else {
             //Set the target position on X axis
@@ -50,6 +65,10 @@ public class Piece : MonoBehaviour
             //Move towards the target
             targetPosition = new Vector2(transform.position.x, targetRow);
             transform.position = Vector2.Lerp(transform.position, targetPosition,17f*Time.deltaTime);
+            if(board.allPieces[column, row] != this.gameObject) {
+                board.allPieces[column, row] = this.gameObject;
+            }
+            
         }
         else {
             //Set the target position on Y axis
@@ -57,16 +76,7 @@ public class Piece : MonoBehaviour
             transform.position = targetPosition;
             board.allPieces[column, row] = this.gameObject;
         }
-        //Code for destruction of matched pieces
         findAllLegalMatches();
-        if (isMatched == true) {
-            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-            sprite.color = new Color(1f, 1f, 1f, 0.4f);
-
-
-        }
-
-        
     }
     private void OnMouseDown() {
         firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -77,58 +87,80 @@ public class Piece : MonoBehaviour
         calculateAngle();
     }
     private void calculateAngle() {
-        swipeAngle = Mathf.Atan2(lastTouchPosition.y - firstTouchPosition.y, lastTouchPosition.x - firstTouchPosition.x) * 180/Mathf.PI;
-        movePieces();
+        if(Mathf.Abs(lastTouchPosition.y - firstTouchPosition.y) > swipeThreshold || Mathf.Abs(lastTouchPosition.x - firstTouchPosition.x) > swipeThreshold  ) {
+            swipeAngle = Mathf.Atan2(lastTouchPosition.y - firstTouchPosition.y, lastTouchPosition.x - firstTouchPosition.x) * 180/Mathf.PI;
+            movePieces();
+        }
     }
     private void movePieces() {
         //Code to the movement of one piece with the adjacent piece from a swipe
         if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1) {
             //Right swipe
-            otherPiece = board.allPieces[column + 1, row];
-            otherPiece.GetComponent<Piece>().column -= 1;
+            secondPiece = board.allPieces[column + 1, row];
+            secondPiece.GetComponent<Piece>().column -= 1;
             column += 1;
         }
         else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.height - 1) {
             //Up swipe
-            otherPiece = board.allPieces[column, row + 1];
-            otherPiece.GetComponent<Piece>().row -= 1;
+            secondPiece = board.allPieces[column, row + 1];
+            secondPiece.GetComponent<Piece>().row -= 1;
             row += 1;
         }
         else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0) {
             //Left swipe
-            otherPiece = board.allPieces[column - 1, row];
-            otherPiece.GetComponent<Piece>().column += 1;
+            secondPiece = board.allPieces[column - 1, row];
+            secondPiece.GetComponent<Piece>().column += 1;
             column -= 1;
         }
         else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0) {
             //Down swipe
-            otherPiece = board.allPieces[column, row - 1];
-            otherPiece.GetComponent<Piece>().row += 1;
+            secondPiece = board.allPieces[column, row - 1];
+            secondPiece.GetComponent<Piece>().row += 1;
             row -= 1;
         }
+        StartCoroutine(checkMoveCoroutine());
+    }
+    public IEnumerator checkMoveCoroutine() {
+        yield return new WaitForSeconds(0.25f);
+        if (secondPiece != null) {
+            if(!isMatched && !secondPiece.GetComponent<Piece>().isMatched) {
+                secondPiece.GetComponent<Piece>().column = column;
+                secondPiece.GetComponent<Piece>().row = row;
+                column = previousColumn;
+                row = previousRow;
+            }
+            else {
+                board.destrolAllMatches();
+            }
+            secondPiece = null;
+        }
+            
+            
         
+
     }
     public void findAllLegalMatches() {
         if (column > 0 && column < board.width - 1) {
             GameObject leftPiece1 = board.allPieces[column - 1, row];
             GameObject rightPiece1 = board.allPieces[column + 1, row];
-            if (leftPiece1.GetComponent<Piece>().color == color && rightPiece1.GetComponent<Piece>().color == color) {
-                leftPiece1.GetComponent<Piece>().isMatched = true;
-                rightPiece1.GetComponent<Piece>().isMatched = true;
-                isMatched = true;
-
-            } 
+            if(leftPiece1 != null && rightPiece1 != null && leftPiece1 != this.gameObject && rightPiece1 != this.gameObject) {
+                if (leftPiece1.GetComponent<Piece>().color == color && rightPiece1.GetComponent<Piece>().color == color) {
+                    leftPiece1.GetComponent<Piece>().isMatched = true;
+                    rightPiece1.GetComponent<Piece>().isMatched = true;
+                    isMatched = true;
+                }
+            }
         }
         if (row > 0 && row < board.height - 1) {
             GameObject downPiece1 = board.allPieces[column, row - 1];
             GameObject upPiece1 = board.allPieces[column, row + 1];
-            if (downPiece1.GetComponent<Piece>().color == color && upPiece1.GetComponent<Piece>().color == color) {
-                downPiece1.GetComponent<Piece>().isMatched = true;
-                upPiece1.GetComponent<Piece>().isMatched = true;
-                isMatched = true;
-
-            } 
+            if(downPiece1 != null && upPiece1 != null && upPiece1 != this.gameObject && downPiece1 != this.gameObject) {
+                if (downPiece1.GetComponent<Piece>().color == color && upPiece1.GetComponent<Piece>().color == color) {
+                    downPiece1.GetComponent<Piece>().isMatched = true;
+                    upPiece1.GetComponent<Piece>().isMatched = true;
+                    isMatched = true;
+                }
+            }
         }
     }
-
 }
